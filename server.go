037da335d225e2b08a5c7259c636bcca7821e728
grugrut <grugruglut+github@gmail.com>
2017-08-29 08:52:20 +0900

    Add scaffold

--- server.go
+++ server.go
@@ -16,6 +16,7 @@ type Wine struct {
 
 func init() {
 	http.HandleFunc("/", pageHandler)
+	http.HandlerFunc("/wine", wineHandler)
 }
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
@@ -37,4 +38,31 @@ func pageHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	fmt.Fprint(w, wines)
+}
+
+func wineHandler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	if u := user.Current(c); u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		//wineGetHandler(w, r)
+		return
+	case "POST":
+		//winePostHandler(w, r)
+		return
+	case "DELETE":
+		//wineDeleteHandler(w, r)
+		return
+	}
+	http.Error(w, "NotFound", http.StatusNotFound)
 }
