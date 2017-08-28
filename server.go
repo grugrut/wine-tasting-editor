11e614ecde8de4867e954561159a76d041c94888
grugrut <grugruglut+github@gmail.com>
2017-08-28 23:57:40 +0900

    Add wine model

--- server.go
+++ server.go
@@ -1,19 +1,40 @@
-package main
+package server
 
 import (
 	"fmt"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/user"
 	"net/http"
 )
 
+// Wine datamodel
+type Wine struct {
+	Name string
+	Year int
+}
+
 func init() {
 	http.HandleFunc("/", pageHandler)
-	http.HandleFunc("/api/", restHandler)
 }
 
 func pageHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world")
-}
+	c := appengine.NewContext(r)
+	if u := user.Current(c); u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
 
-func restHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Rest dummy")
+	q := datastore.NewQuery("Wine").Order("-Name").Limit(10)
+	wines := make([]Wine, 0, 10)
+	if _, err := q.GetAll(c, &wines); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	fmt.Fprint(w, wines)
 }
