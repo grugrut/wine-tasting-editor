de9f6f37597a8727444517785ca5d56ee64bb7c8
grugrut <grugruglut+github@gmail.com>
2017-09-12 23:52:43 +0900

    update

--- src/model/review.go
+++ src/model/review.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"context"
+	"google.golang.org/appengine/datastore"
 	"time"
 )
 
@@ -15,4 +17,21 @@ type Review struct {
 	Updated time.Time
 	Account string
 	WineID  int
+}
+
+func GetReviews(c context.Context) ([]Review, error) {
+	var reviews []Review
+	q := datastore.NewQuery("Review")
+
+	if _, err := q.GetAll(c, &reviews); err != nil {
+		return nil, err
+	}
+
+	return reviews, nil
+}
+
+func InsertReview(c context.Context, r Review) error {
+	key := datastore.NewIncompleteKey(c, "Review", nil)
+	_, err := datastore.Put(c, key, &r)
+	return err
 }
